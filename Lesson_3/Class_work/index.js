3ddc6f6216bc65b76f6d73faa944f7dcca036ab5
Zislin Vasiliy <zislin.maestro@yandex.ru>
2021-10-02 12:27:04 +0300

    Классная работа

---
+++ Lesson_3/Class_work/index.js
@@ -0,0 +1,101 @@
+// const firstValue = String(5);
+// console.log(typeof firstValue);
+
+// const obj = {
+//   foo: "bar",
+// };
+
+// console.log(typeof String(obj));
+// // number
+
+// let testValue = "12345";
+// console.log(typeof testValue);
+
+// testValue = Number(testValue);
+// console.log(typeof testValue);
+
+// // underfined становится NaN
+// let testValue1;
+// console.log(typeof testValue1);
+
+// testValue1 = Number(testValue1);
+// console.log(testValue1);
+
+// const isUser = true;
+// const isAdmin = false;
+// console.log(Number(isUser), Number(isAdmin));
+
+// const voidString = "";
+// console.log(Number(voidString));
+
+// BOOLEAN
+
+// const value = "12";
+// console.log(Boolean(value));
+
+// const obj1 = [1].length;
+// console.log(Boolean(obj1));
+
+// Четные\нечетные
+
+// console.log(16 % 2);
+// console.log(2 ** 2);
+
+// Унарный плюс
+
+// const value = 5;
+// console.log(typeof +value, typeof +5);
+
+// const value = "5";
+// console.log(typeof value, typeof +value, typeof Number(value));
+
+// БИНАРНЫЙ ПЛЮС
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = "12";
+// const numberValue = 34;
+
+// console.log(stringValue + numberValue);
+// console.log(typeof (numberValue + +stringValue));
+
+// const firstName = "Alex";
+// const lastName = "Shirocov";
+// console.log(firstName + " " + lastName);
+// console.log(`${firstName} ${lastName}`);
+
+// ОПЕРАЦИИ СРАВНЕНИЯ
+
+// console.log(false != 5);
+// console.log(false == 0);
+
+// const firstString = "яблоко";
+// const secondString = "Яблоко";
+// console.log(firstString > secondString);
+
+// const a = 0;
+// const b = "0";
+// console.log(a == b);
+// console.log(a === b);
+
+// ПРИМЕРЫ
+
+console.log("2" > "12");
+
+console.log("" - 1 + 0, typeof ("" - 1 + 0));
+
+console.log(true + "" + false);
+console.log(true + false + "");
+
+console.log(6 / "3");
+
+console.log(null == "\n0\n");
+console.log(null === +"\n0\n");
+
+console.log(null === null);
