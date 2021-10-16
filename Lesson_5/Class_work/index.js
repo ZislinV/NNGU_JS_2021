bbd5ca239970452b0e3458ed52d097a4570a29ad
Zislin Vasiliy <zislin.maestro@yandex.ru>
2021-10-16 10:00:50 +0300

    classWork5

---
+++ Lesson_5/Class_work/index.js
@@ -0,0 +1,24 @@
+// const isUserAuth = true;
+// function checkAccess() {
+//   if (isUserAuth) {
+//     console.log("Доступ есть");
+//   } else {
+//     console.log("Доступа нет");
+//   }
+// }
+// checkAccess();
+
+// function writeMessage() {
+//   if (isUserAuth) {
+//     console.log("Привет пользователь");
+//   } else {
+//     console.log("Привет гость");
+//   }
+// }
+// writeMessage();
+
+function sum(a, b) {
+  console.log(a + b);
+}
+sum(1, 0);
+sum(16);
